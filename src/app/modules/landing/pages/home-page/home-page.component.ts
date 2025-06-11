55752f434e97c6b35b73678f5a58a920547844a4
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-06-11 16:44:33 -0500

    ♻️: Update of domains directory by modules

--- src/app/modules/landing/pages/home-page/home-page.component.ts
+++ src/app/modules/landing/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { HeroComponent } from '../../components/hero/hero.component';
+import { OurDesignsComponent } from '../../components/ourDesigns/ourDesigns.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, OurDesignsComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
