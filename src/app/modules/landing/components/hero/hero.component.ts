e58649588523971123366095938ef295d422847e
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-06-11 17:02:25 -0500

    ♻️ refactor: replace span with BadgeComponent in hero component template

--- src/app/modules/landing/components/hero/hero.component.ts
+++ src/app/modules/landing/components/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BadgeComponent } from '@/shared/components/badge/badge.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, BadgeComponent],
   templateUrl: './hero.component.html',
 })
 export class HeroComponent {}
