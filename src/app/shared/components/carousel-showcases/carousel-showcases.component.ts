c36e0d212a88430a2c4ee3e52477b38387b8dffb
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-06-11 19:33:10 -0500

    ✨ feat: implement carousel showcases component with navigation and item display

---
+++ src/app/shared/components/carousel-showcases/carousel-showcases.component.ts
@@ -0,0 +1,37 @@
+import { CarouselContent } from '@/shared/models/carousels';
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, Input, signal } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BadgeComponent } from '@/shared/components/badge/badge.component';
+
+@Component({
+  selector: 'app-carousel-showcases',
+  imports: [FontAwesomeModule, BadgeComponent, CurrencyPipe],
+  templateUrl: './carousel-showcases.component.html',
+})
+export class CarouselShowcasesComponent {
+  @Input() items = signal<CarouselContent[]>([]);
+
+  currentSlide = signal<number>(0);
+
+  nextSlide() {
+    const itemsArray = this.items();
+    if (itemsArray.length > 0) {
+      this.currentSlide.set((this.currentSlide() + 1) % itemsArray.length);
+    }
+  }
+
+  prevSlide() {
+    const itemsArray = this.items();
+    if (itemsArray.length > 0) {
+      this.currentSlide.set(
+        (this.currentSlide() - 1 + itemsArray.length) % itemsArray.length
+      );
+    }
+  }
+
+  itemSelected = computed(() => {
+    const array = this.items();
+    return array.length ? array[this.currentSlide()] : null;
+  });
+}
