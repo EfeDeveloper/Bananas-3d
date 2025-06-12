5fa324b3682ccbd98798ef5aaf40ea10f17e12e1
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-06-11 19:25:24 -0500

    ✨ feat: add additional FontAwesome icons to icon library initialization

--- src/app/icons.config.ts
+++ src/app/icons.config.ts
@@ -3,9 +3,19 @@ import {
   faPrint as fasPrint,
   faPlay as fasPlay,
   faArrowRight as fasArrowRight,
+  faChevronRight as fasChevronRight,
+  faChevronLeft as fasChevronLeft,
+  faImage as fasImage,
 } from '@fortawesome/free-solid-svg-icons';
 
 // This function initializes the FontAwesome icon library with specific icons, import and add the icons here.
 export function initializeIcons(library: FaIconLibrary): void {
-  library.addIcons(fasPrint, fasPlay, fasArrowRight);
+  library.addIcons(
+    fasPrint,
+    fasPlay,
+    fasArrowRight,
+    fasChevronRight,
+    fasChevronLeft,
+    fasImage
+  );
 }
