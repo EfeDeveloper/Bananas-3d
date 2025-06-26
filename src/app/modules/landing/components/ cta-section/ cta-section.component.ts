1f7f0bf3c627b6ca083822901653f1c139d29665
EfeDeveloper <edwinvilla_adsi@outlook.com>
2025-06-26 17:56:20 -0500

    ✨[app]Add CTA section for project inquiries - Introduced a new component for the Call to Action (CTA) section. - Provides users with a prompt to contact for personalized 3D printing quotes. - Enhances user engagement with a visually appealing design and clear call-to-action button.

---
+++ src/app/modules/landing/components/ cta-section/ cta-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-cta-section',
+  imports: [FontAwesomeModule],
+  templateUrl: './ cta-section.component.html',
+})
+export class CtaSectionComponent {}
